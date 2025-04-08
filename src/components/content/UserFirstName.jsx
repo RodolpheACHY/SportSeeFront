cade95a21bc33fd97d0f142b2a1781807c2b0aad
RodolpheACHY <rod23_reseaux@yahoo.fr>
2025-04-08 08:44:02 +0200

    mentorat

--- src/components/content/UserFirstName.jsx
+++ src/components/content/UserFirstName.jsx
@@ -1,27 +1,11 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-const UserFisrtName = ({ userId }) => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch(`http://localhost:3000/user/${userId}`)
-      .then((res) => res.json())
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((err) => console.error("Erreur données utilisateur :", err))
-      .finally(() => setLoading(false));
-  }, [userId]);
-
-  if (loading) return <p>Chargement...</p>;
-
-  const { userInfos } = data;
+const UserFisrtName = () => {
 
   return (
     <div>
-      <h1>Bonjour, {userInfos.firstName} 👋</h1>
+      <h1>Bonjour, Jean 👋</h1>
       <p>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
     </div>
   );
--- src/pages/profil/ProfilPage.jsx
+++ src/pages/profil/ProfilPage.jsx
@@ -1,10 +1,12 @@
 import CardNutriContainer from "../../components/content/CardNutriContainer";
 import DashboardGraphs from "../../components/content/DashboardGraphs";
+import UserFirstName from "../../components/content/UserFirstName";
 import styles from "./profilPage.module.scss";
 
 const ProfilPage= () => {
   return (
-    <div>
+    <div className={styles.profilPage}>
+      <UserFirstName />
       <div className={styles.profilPage__graphsAndNutriContainer}>
         <DashboardGraphs />
         <CardNutriContainer />
