fbaa2cfc7d3a86a84b9f5db2faf1bc63f94306af
Rodolphe ACHY <125242522+RodolpheACHY@users.noreply.github.com>
2025-11-28 19:19:11 +0100

    Add HomePage route to Router component

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page404 from "./pages/page404/Page404";
 import ProfilPage from "./pages/profilPage/ProfilPage";
 import Layout from "./components/layout/Layout";
+import HomePage from "./pages/homePage/HomePage"; 
 
 function Router() {
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} /> 
               <Route path="profil/:id" element={<ProfilPage/>} />
               <Route path="*" element={<Page404 />} />         
           </Route>
