fd6605581c4d6ecb4d6d6b446c24650089bffa8d
Rodolphe ACHY <125242522+RodolpheACHY@users.noreply.github.com>
2025-11-28 12:59:01 +0100

    Create HomePage component with user links

---
+++ src/pages/homePage/HomePage.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <main className="home-container">
+      <h1 className="home-title">Bienvenue sur SportSee</h1>
+      <p className="home-subtitle">
+        Sélectionnez un utilisateur pour afficher son tableau de bord.
+      </p>
+
+      <div className="home-buttons">
+        <Link className="home-btn" to="/profil/12">
+          Voir le profil de Karl (ID 12)
+        </Link>
+
+        <Link className="home-btn" to="/profil/18">
+          Voir le profil de Cecilia (ID 18)
+        </Link>
+      </div>
+    </main>
+  );
+}
