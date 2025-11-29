f129f8734e3616ce34dd332dbad88a68916a019f
RodolpheACHY <rod23_reseaux@yahoo.fr>
2025-11-29 18:52:44 +0100

    nouvelle version config.js avec API_URL pour recup backend deploye

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
-export const USE_MOCKS = true ;
-export const BASE_URL = 'localhost:3000' ;
-//export const BASE_URL = 'http://localhost:3000' ;
-//export const BASE_URL = 'localhostt:9999' ;
-export const DEFAULT_USER = 12 ;
+// On récupère l'URL de l'API si elle existe (mode API)
+export const API_URL = import.meta.env.VITE_API_URL || null;
+
+// Si l'API n'existe pas → on passe automatiquement en mocks
+export const USE_MOCKS = !API_URL;
+
+// Id par défaut quand aucun chemin /profil/:id n’est fourni
+export const DEFAULT_USER = 12;
