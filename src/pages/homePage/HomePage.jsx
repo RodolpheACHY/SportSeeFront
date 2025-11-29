f9a7e8d5884081927c72bb6c64aa1b364373001e
RodolpheACHY <rod23_reseaux@yahoo.fr>
2025-11-29 15:59:56 +0100

    ajout homepage + config router + test nav ===> ok

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -3,18 +3,18 @@ import styles from "./homePage.module.scss";
 
 export default function Home() {
   return (
-    <main className="home-container">
-      <h1 className="home-title">Bienvenue sur SportSee</h1>
-      <p className="home-subtitle">
-        Sélectionnez un utilisateur pour afficher son tableau de bord.
+    <main className={styles.homePage__homeContainer}>
+      <h1 className={styles.homePage__homeTitle}>Bienvenue sur SportSee</h1>
+      <p className={styles.homePage__homeSubtitle}>
+        Choisissez votre profil pour accéder à vos performances.
       </p>
 
-      <div className="home-buttons">
-        <Link className="home-btn" to="/profil/12">
+      <div className={styles.homePage__homeButtons}>
+        <Link className={styles.homePage__homeBtn} to="/profil/12">
           Voir le profil de Karl (ID 12)
         </Link>
 
-        <Link className="home-btn" to="/profil/18">
+        <Link className={styles.homePage__homeBtn} to="/profil/18">
           Voir le profil de Cecilia (ID 18)
         </Link>
       </div>
